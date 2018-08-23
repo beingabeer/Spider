e4040911402a0a330da297291918edcc9f3ff401
Abeer <abeer.sewak@gmail.com>
2018-08-23 12:27:06 +0530

    added functions to append and  delete data on to a file

--- general.py
+++ general.py
@@ -25,3 +25,31 @@ def write_file(path, data):
     f.close()
 
 
+## Add data onto an existing file
+def append_to_file(path, data):
+    with open(path, 'a') as file:
+        file.write(data + '\n')
+
+
+## Delete the contents of a file
+def delete_file_contents(path):
+    with open(path, 'w') as file:
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
