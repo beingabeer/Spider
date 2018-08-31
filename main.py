dc46449e14c9a1d2e803753b2dc330839177dff3
Abeer <abeer.sewak@gmail.com>
2018-08-31 08:41:56 +0530

    final touches

--- main.py
+++ main.py
@@ -4,14 +4,32 @@ from spider import Spider
 from domain import *
 from general import *
 
-PROJECT_NAME = 'github'
-HOMPAGE = 'https://github.com/beingabeer'
+PROJECT_NAME = 'example'
+HOMPAGE = 'https://www.example.com/foo'
 DOMAIN_NAME = get_domain_name(HOMPAGE)
 QUEUE_FILE = PROJECT_NAME + '/queue.txt'
 CRAWLED_FILE = PROJECT_NAME + '/crawled.txt'
 NUMBER_OF_THREADS = 8
 queue = Queue()
 Spider(PROJECT_NAME, HOMPAGE, DOMAIN_NAME)
+
+
+# Create worker threads (will die when the main exits)
+def create_workers():
+    for _ in range(NUMBER_OF_THREADS):
+        t = threading.Thread(target=work)
+        t.daemon = True
+        t.start()
+
+
+
+# Do the next job in the queue
+def work():
+    while True:
+        url = queue.get()
+        Spider.crawl_page(threading.current_thread().name, url)
+        queue.task_done()
+
 
 
 # Each queued link is a new job
@@ -24,8 +42,12 @@ def create_jobs():
 
 
 # Check if there are items in the queue, if so crawl them
-def crawl()
+def crawl():
     queued_links = file_to_set(QUEUE_FILE)
     if len(queued_links) > 0:
         print(str(len(queued_links)) + ' links in the queue')
         create_jobs()
+
+
+create_workers()
+crawl()
